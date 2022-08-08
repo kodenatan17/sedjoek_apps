2c45b256d0707abd16abdb916f224d77f6ae9bb0
kodenatan17 <kodenatan17@gmail.com>
2022-08-08 23:34:52 +0700

    adding Profile UI

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,13 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:sedjoek_apps/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(defaultMargin),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/photo.png',
+                  width: 45,
+                  height: 45,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                width: kDefaultPadding * 1.6,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Galih Wicaksana',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 20,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      'galihwicaksana@gmail.com',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                    ),
+                    Text(
+                      '+6282114155395',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/icons/ico_logout.png',
+                width: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget menuItem(String text) {
+    return Container(
+      margin: const EdgeInsets.only(top: kDefaultMargin * 1.6),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: primaryTextStyle.copyWith(
+              fontSize: 14,
+              fontWeight: medium,
+            ),
+          ),
+          Icon(
+            Icons.chevron_right,
+            color: primaryTextColor,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        decoration: BoxDecoration(color: backgroundColor1),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: kDefaultPadding * 1.6,
+            ),
+            Text(
+              'Akun',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            menuItem('Edit Profil'),
+            menuItem('Upload Data Tambahan'),
+            menuItem('Bantuan'),
+            SizedBox(
+              height: kDefaultPadding * 1.6,
+            ),
+            Text(
+              'General',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            menuItem('Tentang Sedjoek'),
+            menuItem('Kebijakan Privasi'),
+            menuItem('Syarat & Ketentuan'),
+            menuItem('Rate Aplikasi'),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Profile Page"),
+      body: Column(
+        children: [
+          header(),
+          content(),
+        ],
       ),
     );
   }
